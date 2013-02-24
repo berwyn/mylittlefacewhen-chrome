0a74c4e932a029099e0e83e2d8c6519be9236819
Jamison Greeley <atomicrat2552@gmail.com>
2013-02-24 07:07:06 -0700

    Initial stab at grabbing random images

--- popup/popup.js
+++ popup/popup.js
@@ -1,10 +1,16 @@
+// Some constants
 var HOST_BASE = "http://mylittlefacewhen.com/";
 var API_BASE = "api/v3/";
+var RANDOM_LIMIT = 580;
 
+// Our base function
 function main() {
   $('#quilt').isotope({
     itemSelector: '.item'
   });
+  $('#quilt').hide();
+  $('#empty-text').hide();
+  getRandomImages();
   $('#search-bar').keyup(function(event) {
     input = $('#search-bar').val();
     code = event.keyCode || event.which;
@@ -18,29 +24,44 @@ function main() {
   });
 }
 
+// Fetch some random faces from the API
+function getRandomImages() {
+  beginIndex = Math.floor(Math.random() * RANDOM_LIMIT);
+  requestImages('faces/', null, { order_by: "random", id_gte: beginIndex });
+}
+
+// Navigate to a given face on MLFW
 function navigateToFace(id) {
   alert(id);
 }
 
+// Take the JSON body and render the images in it
 function displayImages(json) {
-  for(var i = 0; i < json.objects.length; i++) {
-    element = $('<div/>')
-      .addClass('item')
-      .attr('style', 'background-image:url('+ HOST_BASE + json.objects[i].image + ');');
-    relRatio = element.width() / element.height();
-    if(relRatio >= 1.3) {
-      element.addClass('item-wide');
-    } else if (relRatio <= .7) {
-      element.addClass('item-tall');
+  if(json.objects.length > 0) {
+    $('#empty-text').show();
+  } else {
+    $('#quilt').show();
+    for(var i = 0; i < json.objects.length; i++) {
+      element = $('<div/>')
+        .addClass('item')
+        .attr('style', 'background-image:url('+ HOST_BASE + json.objects[i].image + ');');
+      relRatio = element.width() / element.height();
+      if(relRatio >= 1.3) {
+        element.addClass('item-wide');
+      } else if (relRatio <= .7) {
+        element.addClass('item-tall');
+      }
+      $('#quilt').isotope('insert', element);
     }
-    $('#quilt').isotope('insert', element);
   }
 }
 
+// Log request errors to the console
 function displayError(jqXHR, textStatus, errorThrown) {
   console.log('Error occured: ' + errorThrown);
 }
 
+// Make an API request for faces
 function requestImages(url, args, json) {
   args = args || [];
   json = JSON.stringify(json) || '';
@@ -61,6 +82,8 @@ function requestImages(url, args, json) {
     type: 'GET'
   });
 }
+
+// When the DOM is ready, run main
 $(function() {
   main();
 });
