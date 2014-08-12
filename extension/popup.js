534bfde0aca2a2a7cb9e5c307278962eff82990d
berwyn <berwyn.codeweaver@gmail.com>
2014-08-12 10:56:06 -0600

    Update Angular files based on style guide (and it's cleaner!)

--- extension/popup.js
+++ extension/popup.js
@@ -5,26 +5,54 @@ $(document).ready(function() {
 
 app = angular.module('MLFW', ['akoenig.deckgrid']);
 
-app.config(['$httpProvider', function($httpProvider) {
+var config = function config($httpProvider) {
 	$httpProvider.defaults.useXDomain = true;
 	delete $httpProvider.defaults.headers.common['X-Requested-With'];
-}]);
+}
 
-app.controller('MlfwController', ['$scope', '$http', function($scope, $http) {
-	$scope.getFaces = function getFaces() {
-		console.log('Getting faces - ' + $scope.query);
-		var tags = $scope.query;
-		$http.get('http://mylittlefacewhen.com/api/v3/face', {
-			params: {
-				tags__any: tags
-			}
-		}).success(function(json, status, headers, config) {
-			console.log(json);
-			$scope.faces = json.objects;
-			$scope.noResults = json.objects.length == 0;
-		}).error(function(data, status, headers, config) {
-			var err = "Failed - " + status + ' ' + headers;
-			console.log(err);
-		});
+angular
+	.module('MLFW')
+	.config(config);
+
+var FaceService = function FaceService($http, $q) {
+	var FaceServiceHost = {};
+	FaceServiceHost.getFaces = function getFaces(tags) {
+		var deferred = $q.defer();
+		$http
+			.get('http://mylittlefacewhen.com/api/v3/face', {
+				params: {
+					tags__any: tags
+				}
+			})
+			.success(function(json, status, headers, config) {
+				deferred.resolve(json.objects);
+			})
+			.error(function(data, status, headers, conifg) {
+				deferred.reject(data);
+			});
+			return deferred.promise;
 	};
-}]);
+	return FaceServiceHost;
+};
+
+angular
+	.module('MLFW')
+	.service('FaceService', FaceService);
+
+var MLFWCtrl = function MLFWCtrl(FaceService) {
+	var vm = this;
+
+	vm.getFaces = function getFaces() {
+		console.log('Getting faces - ' + vm.query);
+		var tags = vm.query;
+		FaceService
+			.getFaces(tags)
+			.then(function(faces) {
+				vm.faces = faces;
+			});
+	};
+};
+
+angular
+	.module('MLFW')
+	.controller('MLFWCtrl', MLFWCtrl);
